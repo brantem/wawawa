d7202f857a4f29d113923fef2e37a915bea38b5e
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-03-01 12:30:42 +0700

    Remove scroll from <Player />

--- src/routes/Watch/components/Player.tsx
+++ src/routes/Watch/components/Player.tsx
@@ -6,21 +6,22 @@ import '@vidstack/react/player/styles/default/layouts/video.css';
 
 export default function Player() {
   return (
-    <MediaPlayer
-      className="h-full"
-      title="Sprite Fight"
-      // non HLS
-      src={{
-        src: 'https://files.vidstack.io/sprite-fight/720p.mp4',
-        type: 'video/mp4',
-      }}
-      // HLS
-      // src="https://files.vidstack.io/sprite-fight/hls/stream.m3u8"
-      streamType="on-demand"
-      autoPlay
-    >
-      <MediaProvider />
-      <DefaultVideoLayout icons={defaultLayoutIcons} download={false} />
-    </MediaPlayer>
+    <div className="size-full overflow-hidden">
+      <MediaPlayer
+        title="Sprite Fight"
+        // non HLS
+        src={{
+          src: 'https://files.vidstack.io/sprite-fight/720p.mp4',
+          type: 'video/mp4',
+        }}
+        // HLS
+        // src="https://files.vidstack.io/sprite-fight/hls/stream.m3u8"
+        streamType="on-demand"
+        autoPlay
+      >
+        <MediaProvider />
+        <DefaultVideoLayout icons={defaultLayoutIcons} download={false} />
+      </MediaPlayer>
+    </div>
   );
 }
