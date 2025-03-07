c9d3bb754b267563ed702425bef52b3f28286519
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-03-07 16:30:21 +0700

    Make <StreamingServerUnavailableBanner /> dismissible

--- src/components/StreamingServerUnavailableBanner.tsx
+++ src/components/StreamingServerUnavailableBanner.tsx
@@ -1,21 +1,26 @@
-import { useStreamingServer } from 'lib/hooks';
+import { useState } from 'react';
 import { Link } from 'react-router';
+import { XMarkIcon } from '@heroicons/react/20/solid';
+
+import { useStreamingServer } from 'lib/hooks';
+import { cn } from 'lib/helpers';
 
 export default function StreamingServerUnavailableBanner() {
   const server = useStreamingServer();
 
-  // TODO: dismissible
+  // TODO: persist?
+  const [isHidden, setIsHidden] = useState(false);
 
-  if (server.isLoading || server.settings) return null;
+  if (isHidden || server.isLoading || server.settings) return null;
 
   return (
-    <div className="flex items-end justify-between gap-2 border-l-4 border-yellow-500 py-1 pl-3 max-md:flex-col md:items-center md:gap-4">
+    <div className="relative flex justify-between gap-2 border-l-4 border-yellow-500 py-1 pl-3 max-sm:flex-col sm:items-center sm:gap-4">
       <div>
         <h2 className="font-medium text-yellow-500">Streaming Server Unavailable</h2>
         <p className="text-sm text-neutral-500">Videos might not play because the streaming server is unavailable</p>
       </div>
 
-      <div className="flex items-center gap-2 text-sm font-medium">
+      <div className="flex items-center gap-2 text-sm font-medium max-sm:self-end">
         <Link
           to="/settings#streaming"
           className="rounded-full border border-neutral-800 bg-neutral-900 px-4 py-1 hover:border-neutral-700 hover:bg-neutral-800"
@@ -30,7 +35,23 @@ export default function StreamingServerUnavailableBanner() {
         >
           Install
         </a>
+
+        <CloseButton className="top-px right-0.5 -mr-1.25 max-sm:absolute" onClick={() => setIsHidden(true)} />
       </div>
     </div>
+  );
+}
+
+function CloseButton({ className, onClick }: { className: string; onClick(): void }) {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        'flex size-7.5 items-center justify-center rounded-full stroke-white hover:bg-white hover:stroke-neutral-950 hover:text-neutral-950',
+        className,
+      )}
+    >
+      <XMarkIcon className="size-5" />
+    </button>
   );
 }
